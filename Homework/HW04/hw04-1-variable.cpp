c839fe1356968bc2b5dedf5aad4f7184ff4317f8
jxnipat <Jetnipat.prc@gmail.com>
2023-08-13 18:11:27 +0700

    upppp 4.1

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -5,8 +5,15 @@
         10 20.0 Haha a
 */
 
-int a = 10 ;
-float b = 20 ;
-char c[] = "Haha" ;
-char d = 'a' ;
-printf( "_ _ _ _", _, _, _, _) ; //TODO: #10 Display variable a, b, c and d.
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 20.0;
+    char c[] = "Haha";
+    char d = 'a';
+
+    printf("%d %.1f %s %c\n", a, b, c, d);
+
+    return 0;
+}
